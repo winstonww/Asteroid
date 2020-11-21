308b86d69a2165df2c0bca017c16b7011a733347
Wong Winston <winstonww@Wongs-MacBook-Pro.local>
2020-11-22 00:34:59 +0800

    Add database

--- app/src/main/java/com/udacity/asteroidradar/database/Room.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/Room.kt
@@ -1,2 +1,33 @@
 package com.udacity.asteroidradar.database
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface AsteroidDatabseDao {
+    @Query("SELECT * FROM databaseasteroid")
+    fun getAsteroids(): LiveData<List<DatabaseAsteroid>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAsteroids(vararg asteroids: DatabaseAsteroid)
+}
+
+@Database(entities = [DatabaseAsteroid::class], version = 1)
+abstract class AsteroidDatabase: RoomDatabase() {
+    abstract val dao: AsteroidDatabseDao
+}
+
+private lateinit var INSTANCE : AsteroidDatabase
+
+fun getDatabase(context: Context): AsteroidDatabase {
+    synchronized(AsteroidDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext, AsteroidDatabase::class.java, "asteroids"
+            ).build()
+        }
+    }
+    return INSTANCE
+}
+
